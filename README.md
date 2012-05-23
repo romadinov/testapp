e6519b5f9f759c9a6675df0e3ed6d29825229c3b
Alexey Romadinov <Alexey.Romadinov@gmail.com>
2012-05-23 17:34:16 +1000

    Initial import.

---
+++ README.md
@@ -0,0 +1,27 @@
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* active_admin
+* capybara
+* cucumber
+* git
+* github
+* guard
+* jquery
+* rails_basics
+* rspec
+* twitter_bootstrap
+* simple_form
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '8afb75a497af2de9e954928db135a7c30ac214481b9d6239f8055a241233d6a69b6e0477c252c92541eb9c9af43a9f76ff0616daa1d8aa95cfc9c1290d976551'
